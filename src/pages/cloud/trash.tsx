46cc8f15f2a237dd95bb9ce85138726eee1cc70e
portalcitrus <93584402+portalcitrus@users.noreply.github.com>
2022-11-06 17:23:19 +0300

    feat(page): Добавление страницы "Корзина"

---
+++ src/pages/cloud/trash.tsx
@@ -0,0 +1,9 @@
+import { CloudLayout } from '../../client/components/Cloud/Layout';
+
+export default function Cloud() {
+  return (
+    <CloudLayout title="Корзина">
+      <div style={{ height: '1200px' }} />
+    </CloudLayout>
+  );
+}
