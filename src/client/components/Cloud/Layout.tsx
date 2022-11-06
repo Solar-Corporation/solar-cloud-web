22eae7bff812b9646e2692e1ff7d2c5f701f2259
portalcitrus <93584402+portalcitrus@users.noreply.github.com>
2022-11-06 17:20:17 +0300

    feat(component): Добавление лэйаута файлового менеджера

---
+++ src/client/components/Cloud/Layout.tsx
@@ -0,0 +1,37 @@
+import {
+  DeleteOutlined,
+  FileOutlined,
+  HistoryOutlined,
+} from '@ant-design/icons';
+import { FC, ReactNode } from 'react';
+import styles from '../../styles/components/CloudLayout.module.css';
+import { AppLayout } from '../AppLayout';
+import { INavbarItem, Navbar } from '../Navbar';
+import { ButtonUpload } from '../UI/ButtonUpload';
+
+interface CloudLayoutProps {
+  title: string;
+  children: ReactNode;
+}
+
+export const CloudLayout: FC<CloudLayoutProps> = ({ title, children }) => {
+  const links: INavbarItem[] = [
+    { icon: <HistoryOutlined />, title: 'Недавние', href: '/cloud/recent' },
+    { icon: <FileOutlined />, title: 'Все файлы', href: '/cloud' },
+    { icon: <DeleteOutlined />, title: 'Корзина', href: '/cloud/trash' },
+  ];
+
+  return (
+    <AppLayout
+      title={title}
+      navbar={
+        <div className={styles.navbar}>
+          <ButtonUpload />
+          <Navbar links={links} />
+        </div>
+      }
+    >
+      {children}
+    </AppLayout>
+  );
+};
