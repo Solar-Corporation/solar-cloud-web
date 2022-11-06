20ea6734e6d6df2cc63f69247c0c7a8d70eaa2eb
portalcitrus <93584402+portalcitrus@users.noreply.github.com>
2022-11-06 17:21:16 +0300

    feat(page): Добавление страницы "Все файлы"

---
+++ src/pages/cloud/index.tsx
@@ -0,0 +1,9 @@
+import { CloudLayout } from '../../client/components/Cloud/Layout';
+
+export default function Cloud() {
+  return (
+    <CloudLayout title="Все файлы">
+      <div style={{ height: '1200px' }} />
+    </CloudLayout>
+  );
+}
