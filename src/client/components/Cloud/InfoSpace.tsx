b53f1833af5883c4dc44b15c3a2bb19f15b0fa34
portalcitrus <93584402+portalcitrus@users.noreply.github.com>
2022-11-07 23:47:08 +0300

    fix(component): Переработка блока информации о хранилище

--- src/client/components/Cloud/InfoSpace.tsx
+++ src/client/components/Cloud/InfoSpace.tsx
@@ -1,23 +1,41 @@
+import { CloudServerOutlined } from '@ant-design/icons';
 import { Progress } from 'antd';
 import { FC } from 'react';
 import styles from '../../styles/components/CloudInfoSpace.module.css';
-import { ButtonIncrease } from '../UI/ButtonIncrease';
 
-export const CloudInfoSpace: FC = () => {
+interface CloudInfoSpaceProps {
+  used: number;
+  total: number;
+}
+
+export const CloudInfoSpace: FC<CloudInfoSpaceProps> = ({ used, total }) => {
+  const percent = (used / total) * 100;
+
   return (
     <div className={styles.container}>
-      <div className={styles.indicator}>
-        <Progress
-          strokeColor={{
-            '0%': 'var(--color-calendula-gold)',
-            '100%': 'var(--color-calendula-gold)',
-          }}
-          percent={13.4}
-          showInfo={false}
-        />
-        <span>Осталось 12,16 ГБ из 15 ГБ</span>
+      <div className={styles.title}>
+        <CloudServerOutlined />
+        <span className={styles.titleText}>Хранилище</span>
       </div>
-      <ButtonIncrease />
+      <div className={styles.info}>
+        <span className={styles.space}>{used} ГБ </span>|
+        <span className={styles.space}> </span>
+        {total} ГБ
+      </div>
+      <Progress
+        strokeColor={
+          percent > 35
+            ? percent > 70
+              ? percent > 99
+                ? 'var(--color-red)'
+                : 'var(--color-magenta)'
+              : 'var(--color-sunset-orange)'
+            : 'var(--color-calendula-gold)'
+        }
+        trailColor={'rgba(0, 0, 0, 0.2)'}
+        percent={percent}
+        showInfo={false}
+      />
     </div>
   );
 };
