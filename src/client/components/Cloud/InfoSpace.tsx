1d7ac89f00b4bdb65d29a12c87dc8ba22f08a25f
Стефан Валетов <stormerxl24@gmail.com>
2022-11-07 20:15:49 +0300

    feat(component): Добавление блока информации о хранилище

---
+++ src/client/components/Cloud/InfoSpace.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import styles from '../../styles/components/CloudInfoSpace.module.css';
+import { ButtonIncrease } from '../UI/ButtonIncrease';
+import { Progress } from 'antd';
+
+export const CloudInfoSpace: FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.indicator}>
+        <Progress
+          strokeColor={{
+            '0%': 'var(--color-calendula-gold)',
+            '100%': 'var(--color-calendula-gold)',
+          }}
+          percent={13.4}
+          showInfo={false}
+        />
+        <span>Осталось 12,16 ГБ из 15 ГБ</span>
+      </div>
+      <ButtonIncrease />
+    </div>
+  );
+};
