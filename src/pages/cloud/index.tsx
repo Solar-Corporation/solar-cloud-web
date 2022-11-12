48c6eb84715ab44859fc56f537103389fec84fd9
leankee <leankee217@gmail.com>
2022-11-12 23:59:26 +0300

    fix(page): Обновление props

--- src/pages/cloud/index.tsx
+++ src/pages/cloud/index.tsx
@@ -1,17 +1,21 @@
-import { IBreadcrumbsItem } from '../../client/components/Breadcrumbs';
 import { CloudLayout } from '../../client/components/Cloud/Layout';
-import { PageHeading } from '../../client/components/PageHeading';
+import { Action } from '../../client/components/UI/Action';
+import { Control } from '../../client/components/UI/Control';
+import { RouteNames } from '../../client/router';
 
 export default function Cloud() {
-	const links: IBreadcrumbsItem[] = [
-		{ title: 'Все файлы', href: '/cloud' },
-		{ title: 'Какая-то папка', href: '/cloud/123' },
-		{ title: 'Папка', href: '/cloud/143' }
-	];
+	const headingOptions = {
+		links: [{ title: 'Все файлы', href: RouteNames.CLOUD }],
+		actions: [Action.CREATE],
+		constControls: [Control.VIEW, Control.INFO],
+		sticky: true
+	};
 
 	return (
-		<CloudLayout title="Все файлы">
-			<PageHeading links={links} />
+		<CloudLayout
+			title="Все файлы"
+			headingOptions={headingOptions}
+		>
 			<div style={{ height: '1200px' }} />
 		</CloudLayout>
 	);
