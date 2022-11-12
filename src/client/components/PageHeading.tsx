4dae4117f5e8eb102e00f895ac459a505ea2dc60
Стефан Валетов <stormerxl24@gmail.com>
2022-11-12 14:20:27 +0300

    feat(component): Добавление заголовка страницы

---
+++ src/client/components/PageHeading.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from 'react';
+import { Breadcrumbs, IBreadcrumbsItem } from './Breadcrumbs';
+
+interface PageHeadingProps {
+  links: IBreadcrumbsItem[];
+  actions?: ReactNode;
+}
+
+export const PageHeading: FC<PageHeadingProps> = ({ links, actions }) => {
+  return (
+    <div>
+      <Breadcrumbs links={links} controls={actions} />
+    </div>
+  );
+};
