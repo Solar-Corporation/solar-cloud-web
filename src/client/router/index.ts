a68dcfddc1ba1a2ce30348999acc5493ccddd5ee
leankee <leankee217@gmail.com>
2022-11-12 17:00:07 +0300

    feat(router): Добавление роутера

---
+++ src/client/router/index.ts
@@ -0,0 +1,11 @@
+
+
+export enum RouteNames {
+	HOME = '/',
+	CLOUD = '/cloud',
+	RECENT = '/cloud/recent',
+	MARKED = '/cloud/marked',
+	TAGS = '/cloud/tags',
+	TRASH = '/cloud/trash',
+	SETTINGS = '/settings'
+}
