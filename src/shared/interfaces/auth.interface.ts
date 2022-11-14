1b8f656d0457b3a25864a7ac747c9516c534724e
dan_kos <kosenko.dv@e5ng.ru>
2022-11-14 23:52:55 +0300

    feat(auth): Обновление интерфейса авторизации

--- src/shared/interfaces/auth.interface.ts
+++ src/shared/interfaces/auth.interface.ts
@@ -1,16 +1,23 @@
 import { Transaction } from 'sequelize';
-import { DeviceDataDto, RegistrationUserDto } from '../../server/auth/dto';
+import { DeviceDataDto, UserRegistrationDto } from '../../server/auth/dto';
 import { JwtToken } from '../types/auth.type';
 
 export interface IAuth<T> {
-	registration(registrationUserDto: RegistrationUserDto, deviceDataDto: DeviceDataDto, transaction: Transaction): Promise<JwtToken>;
-
-	login(
-		loginOptions: T,
+	registration(
+		registrationUserDto: UserRegistrationDto,
+		deviceDataDto: DeviceDataDto,
 		transaction: Transaction,
 	): Promise<JwtToken>;
 
-	logout(refresh: string, transaction: Transaction): Promise<void>;
+	login(
+		loginOptions: T,
+		deviceDataDto: DeviceDataDto,
+		transaction: Transaction,
+	): Promise<JwtToken>;
 
-	refresh(refresh: string, transaction: Transaction): Promise<JwtToken>;
+	logout(
+		refresh: string,
+		transaction: Transaction): Promise<void>;
+
+	refresh(refresh: string): Promise<JwtToken>;
 }
