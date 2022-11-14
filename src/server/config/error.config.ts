10e659ccb9436d51a93c6eb3d1189a7ba651d1e5
dan_kos <kosenko.dv@e5ng.ru>
2022-11-15 00:06:56 +0300

    feat(config): Добавление описания новых ошибок

--- src/server/config/error.config.ts
+++ src/server/config/error.config.ts
@@ -3,4 +3,16 @@ export const ErrorsConfig = {
 		message: 'User already exist!',
 		description: 'A user with this email already exists, check the spelling of the email.',
 	},
+	notFound: {
+		message: 'Email does not exist!',
+		description: 'Check the spelling of the email.',
+	},
+	wrongPassword: {
+		message: 'Password is wrong!',
+		description: 'Check your password!',
+	},
+	unauthorized: {
+		message: 'User is not Unauthorized!',
+		description: 'Unauthorized in account please.',
+	},
 };
