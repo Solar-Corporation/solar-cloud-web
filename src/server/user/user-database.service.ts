149074870ffa37d072bdef67bdaf3022bad6217a
dan_kos <kosenko.dv@e5ng.ru>
2022-11-14 23:55:55 +0300

    feat(user): Добавлено получение пользователя по email из БД

--- src/server/user/user-database.service.ts
+++ src/server/user/user-database.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Transaction } from 'sequelize';
-import { RegistrationUserDto } from '../auth/dto';
+import { UserRegistrationDto } from '../auth/dto';
 import { SequelizeConnect } from '../database/database-connect';
-import { UserDto } from './dto/user.dto';
+import { UserAuthDto, UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserDatabaseService {
@@ -12,7 +12,7 @@ export class UserDatabaseService {
 		return isExist[0][0].check_user_by_email;
 	}
 
-	async createUser(createUserDto: RegistrationUserDto, transaction: Transaction): Promise<UserDto> {
+	async createUser(createUserDto: UserRegistrationDto, transaction: Transaction): Promise<UserDto> {
 		const userId: any = await SequelizeConnect.query(`SELECT user_data.add_user('${JSON.stringify(createUserDto)}')`,
 			{ transaction: transaction });
 		return userId[0][0].add_user;
