026a1ff1c8da323098d5cfca4dcc43bb10f3a8fd
leankee <leankee217@gmail.com>
2022-11-15 22:29:30 +0300

    feat(page): Добавление страницы авторизации

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from 'antd';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../../client/img/logo.svg';
+import styles from '../../client/styles/pages/Home.module.css';
+
+export default function Login() {
+	return (
+		<>
+			<Head>
+				<title>SolarCloud</title>
+				<meta name="description" content="description" />
+				<meta charSet="utf-8" />
+			</Head>
+			<main className={styles.main}>
+				<div className={styles.container}>
+					<Image src={Logo} alt="" className={styles.image} />
+					<Link href="/cloud">
+						<Button type="primary" size="large">
+							Перейти в облако
+						</Button>
+					</Link>
+				</div>
+			</main>
+		</>
+	);
+}
