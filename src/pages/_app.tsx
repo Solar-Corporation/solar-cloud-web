f93e3a38ddef58929f65fe2a0a4305f3c039c037
leankee <leankee217@gmail.com>
2022-11-15 22:15:53 +0300

    feat(less): Переработка темы компонентов

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
-import '../client/styles/globals.css';
+// import '../client/styles/globals.css';
+import '../client/styles/globals.less';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
