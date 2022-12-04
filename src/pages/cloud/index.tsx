4400844ef6517d8d4583f8c33195183a5c1cb7de
Стефан Валетов <stormerxl24@gmail.com>
2022-12-04 15:47:32 +0300

    refactor(code): Чистка кода

--- src/pages/cloud/index.tsx
+++ src/pages/cloud/index.tsx
@@ -2,8 +2,12 @@ import { CloudLayout } from '../../client/components/Cloud/Layout';
 import { Action } from '../../client/components/UI/Action/List';
 import { Control } from '../../client/components/UI/Control/List';
 import { RouteNames } from '../../client/router';
+import { useRouter } from 'next/router';
 
 export default function Cloud() {
+	const id = 3432432;
+	const router = useRouter();
+
 	const headingOptions = {
 		links: [{ title: 'Все файлы', href: RouteNames.CLOUD }],
 		actions: [Action.CREATE],
@@ -11,11 +15,16 @@ export default function Cloud() {
 		sticky: true
 	};
 
+	const handleClick = async () => {
+		await router.push(`${RouteNames.CLOUD}/?folder=${id}`);
+	};
+
 	return (
 		<CloudLayout
 			title="Все файлы"
 			headingOptions={headingOptions}
 		>
+			<button onClick={handleClick}>Открыть папку</button>
 			<div style={{ height: '1200px' }} />
 		</CloudLayout>
 	);
