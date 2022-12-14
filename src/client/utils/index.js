5ee6ff95bc7cac98247dcfefa7d8f8770269804b
leankee <leankee217@gmail.com>
2022-12-15 01:28:49 +0300

    refactor(code): Чистка кода

--- src/client/utils/index.js
+++ src/client/utils/index.js
@@ -1,6 +1,6 @@
 export const tooltipShowDelay = 0.4;
 
-export async function hash(string: string) {
+export async function hash(string) {
 	const utf8 = new TextEncoder().encode(string);
 	const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
 	const hashArray = Array.from(new Uint8Array(hashBuffer));
