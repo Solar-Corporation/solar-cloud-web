be664020287a9a078fcc694b0f002a36d5a49e4d
dan_kos <kosenko.dv@e5ng.ru>
2022-12-22 05:34:13 +0300

    feat(file-service): Добавлена работа c файлами через Rust
    Добавлено
    - Загрузка файлов
    - Получение файла
    - Получение списка файлов в директории
    - Переименование файла

--- src/server/file-rs/file_service.rs
+++ src/server/file-rs/file_service.rs
@@ -1,8 +1,11 @@
 use std::path::{Path, PathBuf};
+use std::time::SystemTime;
 
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
 use xattr::set;
+
+use file_system::FileSystem;
 
 mod file_system;
 
@@ -10,6 +13,27 @@ mod file_system;
 pub struct UserDir {
 	pub uuid: String,
 	pub storage: i64,
+}
+
+#[napi(object)]
+pub struct UserFile {
+	pub buffer: Buffer,
+	pub file_path: String,
+	pub mime_type: Option<String>,
+	pub see_time: Option<i64>,
+	pub size: Option<i64>,
+}
+
+#[napi(object)]
+pub struct FileTree {
+	pub name: String,
+	pub path: String,
+	pub size: String,
+	pub file_type: String,
+	pub mime_type: String,
+	pub is_dir: bool,
+	pub is_favorite: bool,
+	pub see_time: i64,
 }
 
 #[napi]
@@ -30,9 +54,80 @@ impl FileService {
 
 		let user_dir = Path::new(&base_path).join(user.uuid);
 		file_system::FileSystem::create_dir(&user_dir).await?;
-		set(&user_dir, "available_space", (user.storage as u64).to_string().as_bytes())
-			.expect("Cannot insert system value!");
+		set(&user_dir, "available_space", (user.storage as u64).to_string().as_bytes());
 
 		return Ok(user_dir.into_os_string().into_string().unwrap());
+	}
+
+	/// Метод сохраняет файл в папку пользователя
+	#[napi]
+	pub async fn save_file(file: UserFile, uuid: String, base_path: String) -> Result<()> {
+		let file_path = &PathBuf::from(&file.file_path);
+		let is_exist = FileSystem::is_exist(file_path).await?;
+		if is_exist {
+			return Err(Error::new(Status::Cancelled, "File already exist!".to_owned()));
+		}
+
+		let user_dir = Path::new(&base_path).join(uuid);
+		let available_size = FileSystem::available_size(&user_dir).await?;
+
+		let total_size = FileSystem::total_size(&user_dir).await?;
+		let file_size = file.buffer.len() as u64;
+		if available_size - total_size < file_size {
+			return Err(Error::new(Status::Cancelled, "No free space".to_owned()));
+		}
+
+		file_system::FileSystem::save_file(file.buffer, file_path).await?;
+
+		let size = total_size + file_size;
+		set(&user_dir, "usage_space", size.to_string().as_bytes());
+
+		return Ok(());
+	}
+
+	/// Метод получения файла по пути
+	#[napi]
+	pub async fn get_file(path: String) -> Result<UserFile> {
+		let file_path = &PathBuf::from(&path);
+
+		let is_exist = FileSystem::is_exist(&file_path).await?;
+		if !is_exist {
+			return Err(Error::new(Status::Cancelled, "The file does not exist".to_owned()));
+		}
+
+		let is_delete = FileSystem::is_file_delete(&file_path).await?;
+		if is_delete {
+			return Err(Error::new(Status::Cancelled, "The file has been deleted!".to_owned()));
+		}
+
+		let user_file = FileSystem::get_file(&file_path).await?;
+		return Ok(user_file);
+	}
+
+	/// Метод возвращает файлы, которые находятся в директории по заданному пути
+	#[napi]
+	pub async fn get_file_tree(path: String, base_path: String) -> Result<Vec<FileTree>> {
+		let dir_path = &PathBuf::from(&path);
+
+		let is_exist = FileSystem::is_exist(&dir_path).await?;
+		if !is_exist {
+			return Err(Error::new(Status::Cancelled, "The file does not exist".to_owned()));
+		}
+		let file_tree = FileSystem::file_tree(dir_path, &PathBuf::from(&base_path)).await?;
+		return Ok(file_tree);
+	}
+
+	#[napi]
+	pub async fn rename(path: String, new_path: String) -> Result<()> {
+		let old_path = &PathBuf::from(&path);
+
+		let is_exist = FileSystem::is_exist(&old_path).await?;
+		if !is_exist {
+			return Err(Error::new(Status::Cancelled, "The file does not exist".to_owned()));
+		}
+
+		FileSystem::rename(&old_path, &PathBuf::from(new_path)).await?;
+
+		return Ok(());
 	}
 }
--- src/server/file/file.service.ts
+++ src/server/file/file.service.ts
@@ -1,19 +1,16 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { promises as fs } from 'fs';
 import path from 'path';
 import { Transaction } from 'sequelize';
 import { Readable } from 'stream';
 
-import { FileSystem } from '../../../index';
-import { IFile } from '../../shared/interfaces/file.interface';
-import { FileTree } from '../../shared/types/file.type';
+import { FileService as RsFileService, FileTree, UserFile } from '../../../index';
 import { UserDto } from '../user/dto/user.dto';
 import { FileDto, FileUploadDto, RenameFileDto } from './dto/file.dto';
 import { FileDatabaseService } from './file-database.service';
 
 @Injectable()
-export class FileService implements IFile {
+export class FileService {
 
 	constructor(
 		private readonly fileDatabaseService: FileDatabaseService,
@@ -21,104 +18,48 @@ export class FileService implements IFile {
 	) {
 	}
 
-	async saveFile(fileUploadDto: FileUploadDto, userDto: UserDto, transaction: Transaction): Promise<number> {
-		const { file: { size, buffer, originalName, mimeType }, filePath } = fileUploadDto;
+	async saveFile(fileUploadDto: FileUploadDto, userDto: UserDto): Promise<void> {
+		const { file: { buffer, originalName }, filePath } = fileUploadDto;
 		const storePath = this.configService.get('app.path');
 
-		const fileDto: FileDto = {
+		const userFile: UserFile = {
 			filePath: path.join(storePath, userDto.uuid, filePath, originalName),
-			sizeActual: size,
-			sizeCompressed: size,
-			fileMime: mimeType,
+			buffer: buffer,
 		};
 
-		const isExist = await FileSystem.isFileExist(fileDto.filePath);
-		if (isExist)
-			throw new ConflictException('File already exist!', 'Please rename, file');
-
-		const fileId = await this.fileDatabaseService.addFile(fileDto, userDto.id, transaction);
-		await FileSystem.saveFile(buffer, fileDto.filePath);
-		return fileId;
+		await RsFileService.saveFile(userFile, userDto.uuid, storePath);
 	}
 
 	async getFile(shortFilePath: string, userDto: UserDto): Promise<FileDto> {
 		const storePath = this.configService.get('app.path');
 		const fullFilePath = path.join(storePath, userDto.uuid, shortFilePath);
 
-		const isExist = await FileSystem.isFileExist(fullFilePath);
-		if (!isExist)
-			throw new NotFoundException('File doesn\'t exist!', 'File doesn\'t exist!');
-
-		const fileDto = await this.fileDatabaseService.getFile(fullFilePath);
-		const buffer = await FileSystem.getFile(fullFilePath);
-		fileDto.stream = Readable.from(buffer) as Readable;
-
-		fileDto.type = path.extname(fullFilePath);
-		fileDto.name = path.basename(fullFilePath);
-
-		return fileDto;
+		const userFile = await RsFileService.getFile(fullFilePath);
+		return {
+			fileMime: userFile.mimeType!,
+			filePath: shortFilePath,
+			isDelete: false,
+			name: path.basename(fullFilePath),
+			sizeActual: userFile.size!,
+			stream: Readable.from(userFile.buffer)!,
+			type: path.extname(fullFilePath),
+			updateAt: new Date(userFile.seeTime!),
+		};
 	}
 
 	async renameFile(renameFileDto: RenameFileDto, userDto: UserDto, transaction: Transaction): Promise<void> {
 		const storePath = this.configService.get('app.path');
 		const fullPath = path.join(storePath, userDto.uuid, renameFileDto.filePath);
-
-		const isExist = await FileSystem.isFileExist(fullPath);
-		if (!isExist)
-			throw new NotFoundException('File doesn\'t exist!', 'File doesn\'t exist!');
-
 		const renameFilePath = fullPath.replace(path.basename(fullPath), renameFileDto.newName);
 		await this.fileDatabaseService.updateFilePath({ filePath: fullPath, newName: renameFilePath }, transaction);
-
-		await FileSystem.rename(fullPath, renameFilePath);
+		await RsFileService.rename(fullPath, renameFilePath);
 	}
 
-	async getFileTree(userDto: UserDto): Promise<FileTree> {
-		const storePath = this.configService.get('app.path');
-		const userFilesPath = path.join(storePath, userDto.uuid);
-		return await this.getDirTree(userFilesPath);
-	}
-
-	private async getDirTree(dirPath: string, fileTree =
-		{
-			name: '/',
-			path: '/',
-			isFolder: true,
-			expand: true,
-			children: Array(),
-		}, root: string | null = null,
-	): Promise<FileTree> {
-		const list: Array<string> = await fs.readdir(dirPath);
-		for (let item of list) {
-
-			const itemPath = path.join(dirPath, item);
-			let stats = await fs.stat(itemPath);
-			const rootPath = (root) ? path.join(root, item) : item;
-
-			if (stats.isDirectory()) {
-				const directoryChildren: FileTree = {
-					name: item,
-					path: rootPath,
-					isFolder: true,
-					expand: false,
-					children: [],
-				};
-
-				fileTree.children.push(directoryChildren);
-				const childrenIndex: number = fileTree.children.length - 1;
-				await this.getDirTree(itemPath, fileTree.children[childrenIndex], rootPath);
-				continue;
-			}
-			const fileChildren = {
-				name: item,
-				path: rootPath,
-				isFolder: false,
-				expand: false,
-			};
-			fileTree.children.push(fileChildren);
-
-		}
-		return fileTree;
+	async getFileTree(userDto: UserDto, filePath: string): Promise<Array<FileTree>> {
+		const { uuid } = userDto;
+		const basePath = this.configService.get('app.path');
+		const userFilesPath = path.join(basePath, userDto.uuid, filePath);
+		return await RsFileService.getFileTree(userFilesPath, path.join(basePath, uuid));
 	}
 
 }
