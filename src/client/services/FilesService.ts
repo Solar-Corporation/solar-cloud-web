42ab016b5a303d08dd4fd957ebff4c4a3537787f
leankee <leankee217@gmail.com>
2022-12-28 01:02:27 +0300

    feat(headers): Добавление подставления Headers в запросы

--- src/client/services/FilesService.ts
+++ src/client/services/FilesService.ts
@@ -5,14 +5,21 @@ import {
 	fetchBaseQuery,
 	FetchBaseQueryError
 } from '@reduxjs/toolkit/dist/query/react';
+import { RootState } from '../store';
 import { authAPI } from './AuthService';
 import { apiUrl } from './config';
 
 
-const baseQuery = fetchBaseQuery({ baseUrl: `${apiUrl}/files` });
-const baseQueryWithRefresh: BaseQueryFn<string | FetchArgs,
-	unknown,
-	FetchBaseQueryError> = async (args, api, extraOptions) => {
+const baseQuery = fetchBaseQuery({
+	baseUrl: `${apiUrl}`,
+	prepareHeaders: (headers, { getState }) => {
+		const token = (getState() as RootState).userReducer.token;
+		if (token) headers.set('Authorization', `Bearer ${token}`);
+		return headers;
+	}
+});
+
+const baseQueryWithRefresh: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (args, api, extraOptions) => {
 	let result = await baseQuery(args, api, extraOptions);
 	if (result.error && result.error.status === 401) {
 		// try to get a new token
@@ -29,9 +36,9 @@ export const filesAPI = createApi({
 	reducerPath: 'filesAPI',
 	baseQuery: baseQueryWithRefresh,
 	endpoints: (build) => ({
-		getFiles: build.query<any, any>({
+		getFiles: build.query<any, string>({
 			query: (data) => ({
-				url: ''
+				url: '/files'
 			})
 		})
 	})
