b615dbe26d4ee09cffe36d9b48b4a9fc1bbd5901
leankee <leankee217@gmail.com>
2022-12-28 01:00:43 +0300

    feat(font): Добавление подгрузки шрифта

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,27 @@
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import { authAPI } from '../client/services/AuthService';
 import { wrapper } from '../client/store';
 import '../client/styles/globals.less';
 import { themeCloud } from '../client/styles/theme';
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({ subsets: ['latin', 'cyrillic'], weight: ['400', '500'] });
 
 function App({ Component, ...rest }: AppProps) {
 	const { store, props } = wrapper.useWrappedStore(rest);
-	const { userReducer: { token } } = store.getState();
-
-	if (!token) {
-		store.dispatch(authAPI.endpoints.userRefresh.initiate(null));
-	}
+	// const { userReducer: { token } } = store.getState();
+	//
+	// if (!token) {
+	// 	store.dispatch(authAPI.endpoints.userRefresh.initiate(null));
+	// }
 
 	return (
 		<Provider store={store}>
 			<ConfigProvider theme={themeCloud}>
-				<Component {...props.pageProps} />
+				<main className={roboto.className}>
+					<Component {...props.pageProps} />
+				</main>
 			</ConfigProvider>
 		</Provider>
 	);
