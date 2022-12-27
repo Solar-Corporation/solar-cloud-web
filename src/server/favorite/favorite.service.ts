0286e16a95f1670651920f5af3832d3cdddd8cb4
dan_kos <kosenko.dv@e5ng.ru>
2022-12-27 08:02:56 +0300

    feat(favorite): Добавлен функционал работы с любимыми файлами
    Добавлен
    - Загрузка нескольких файлов
    - проверка на отсутствие абсолютного пути при удалении
    - проверка по копированию по тому же пути

---
+++ src/server/favorite/favorite.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import path from 'path';
+import { Transaction } from 'sequelize';
+import { FileService as RsFileService, FileTree } from '../../../index';
+import { UserDto } from '../user/dto/user.dto';
+import { FavoriteDatabaseService } from './favorite-database.service';
+
+@Injectable()
+export class FavoriteService {
+
+	private readonly basePath;
+
+	constructor(
+		private readonly configService: ConfigService,
+		private readonly favoriteDatabaseService: FavoriteDatabaseService,
+	) {
+		this.basePath = this.configService.get('app.path');
+	}
+
+	async setFavorite({ uuid, id }: UserDto,
+	                  favorites: Array<string>,
+	                  state: boolean,
+	                  transaction: Transaction,
+	): Promise<void> {
+		for (const favorite of favorites) {
+			const fullPath = path.join(this.basePath, uuid, favorite);
+
+			if (state)
+				await this.favoriteDatabaseService.addFavorite(id, fullPath, transaction);
+			else
+				await this.favoriteDatabaseService.deleteFavorite(id, fullPath, transaction);
+
+			await RsFileService.setFavorite(fullPath, state);
+
+		}
+	}
+
+	async getFavorites({ id, uuid }: UserDto): Promise<Array<FileTree>> {
+		const fullPath = path.join(this.basePath, uuid);
+		const paths = await this.favoriteDatabaseService.getFavoritesPaths(id);
+		return await RsFileService.getFilesMetadata(paths, fullPath);
+	}
+}
