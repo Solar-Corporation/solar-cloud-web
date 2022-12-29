faa7ff3e65b68433c1b2e152f6a05dc1aff6af3c
dan_kos <kosenko.dv@e5ng.ru>
2022-12-30 02:02:46 +0300

    fix(auth): Исправление получения данных о пользователе из БД

--- src/server/database/sql/user_data/routines/get_user_by_id.sql
+++ src/server/database/sql/user_data/routines/get_user_by_id.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION get_user_by_id(id integer) RETURNS jsonb
+CREATE OR REPLACE FUNCTION user_data.get_user_by_id(id integer) RETURNS jsonb
 	LANGUAGE plpgsql
 AS
 $$
@@ -14,7 +14,7 @@ BEGIN
 	SELECT JSONB_BUILD_OBJECT('id', user_row.user_id)
 		       || JSONB_BUILD_OBJECT('email', user_row.email_value)
 		       || JSONB_BUILD_OBJECT('nickname', user_row.user_nickname)
-		       || JSONB_BUILD_OBJECT('uuid', user_row.user_nickname)
+		       || JSONB_BUILD_OBJECT('uuid', user_row.user_uuid)
 		       || JSONB_BUILD_OBJECT('fullName', JSONB_BUILD_OBJECT('firstName', user_row.user_first_name,
 		                                                            'middleName', user_row.user_middle_name,
 		                                                            'lastName', user_row.user_last_name))
