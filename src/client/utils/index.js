f934c6020cfc15cdaf1c149d0a7cceb1e38acfb5
leankee <leankee217@gmail.com>
2023-01-06 17:15:29 +0300

    fix(auth): Удаление хеширования

--- src/client/utils/index.js
+++ src/client/utils/index.js
@@ -1,13 +1,13 @@
 export const tooltipShowDelay = 0.4;
 
-export async function hash(string) {
-  const utf8 = new TextEncoder().encode(string);
-  const hashBuffer = await crypto.subtle.digest("SHA-256", utf8);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray
-    .map((bytes) => bytes.toString(16).padStart(2, "0"))
-    .join("");
-}
+// export async function hash(string) {
+//   const utf8 = new TextEncoder().encode(string);
+//   const hashBuffer = await crypto.subtle.digest("SHA-256", utf8);
+//   const hashArray = Array.from(new Uint8Array(hashBuffer));
+//   return hashArray
+//     .map((bytes) => bytes.toString(16).padStart(2, "0"))
+//     .join("");
+// }
 
 // export const getServerSideRefresh: GetServerSideProps = wrapper.getServerSideProps(
 // 	(store) =>
