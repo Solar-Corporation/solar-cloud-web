a29a8aba702eacbfec822accb8fe4a997f5200b0
leankee <leankee217@gmail.com>
2023-01-09 21:49:39 +0300

    feat(hydrate): Added hydrate action

--- src/client/store/reducers/UserSlice.ts
+++ src/client/store/reducers/UserSlice.ts
@@ -1,4 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import jwt from 'jwt-decode';
+import { GetServerSidePropsContext } from 'next';
+import { HYDRATE } from 'next-redux-wrapper';
+import { parseCookies } from 'nookies';
+import { AnyAction } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
 import { IUser } from '../../models/IUser';
 
 export interface UserState {
@@ -19,8 +25,26 @@ export const UserSlice = createSlice({
 			state.data = action.payload.data;
 			state.token = action.payload.token;
 		}
+	},
+	extraReducers: {
+		[HYDRATE]: (state, action) => {
+			return {
+				...state,
+				...action.payload.userReducer
+			};
+		}
 	}
 });
 
 export default UserSlice.reducer;
 export const { setUser } = UserSlice.actions;
+export const setInitialUserData = (ctx: GetServerSidePropsContext, dispatch: ThunkDispatch<any, any, AnyAction>) => {
+	const { accessToken: token } = parseCookies(ctx);
+	if (token) {
+		const user: UserState = {
+			data: jwt(token),
+			token
+		};
+		dispatch(setUser(user));
+	}
+};
