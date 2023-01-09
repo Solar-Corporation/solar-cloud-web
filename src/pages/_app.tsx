1cf4bfc5807bd6a138fd92a25f1163ddafa84987
leankee <leankee217@gmail.com>
2023-01-09 20:18:27 +0300

    feat(favicon): Added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,22 @@
+import { Roboto } from '@next/font/google';
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { wrapper } from '../client/store';
 import '../client/styles/globals.less';
 import { themeCloud } from '../client/styles/theme';
-import { Roboto } from '@next/font/google';
 
 const roboto = Roboto({ subsets: ['latin', 'cyrillic'], weight: ['400', '500'] });
 
 function App({ Component, ...rest }: AppProps) {
 	const { store, props } = wrapper.useWrappedStore(rest);
-	// const { userReducer: { token } } = store.getState();
-	//
-	// if (!token) {
-	// 	store.dispatch(authAPI.endpoints.userRefresh.initiate(null));
-	// }
 
 	return (
 		<Provider store={store}>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<ConfigProvider theme={themeCloud}>
 				<main className={roboto.className}>
 					<Component {...props.pageProps} />
