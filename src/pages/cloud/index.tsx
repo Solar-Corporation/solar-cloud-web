6da1f78758eacf411fc4f85990ad5555feee2bb1
leankee <leankee217@gmail.com>
2023-01-11 00:30:00 +0300

    feat(component): Added File Table to the Main page

--- src/pages/cloud/index.tsx
+++ src/pages/cloud/index.tsx
@@ -1,6 +1,7 @@
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
 import { CloudLayout } from '../../client/components/Cloud/Layout';
+import { FileTable } from '../../client/components/FileTable';
 import { Action } from '../../client/components/UI/Action/List';
 import { Control } from '../../client/components/UI/Control/List';
 import { RouteNames } from '../../client/router';
@@ -8,7 +9,7 @@ import { filesAPI } from '../../client/services/FilesService';
 import { wrapper } from '../../client/store';
 import { setInitialUserData } from '../../client/store/reducers/UserSlice';
 
-export default function Cloud() {
+export default function Cloud({ files }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	const id = 3432432;
 	const router = useRouter();
 
@@ -29,7 +30,7 @@ export default function Cloud() {
 			headingOptions={headingOptions}
 		>
 			{/*<button onClick={handleClick}>Открыть папку</button>*/}
-			<div style={{ height: '1200px' }} />
+			{files && <FileTable files={files} />}
 		</CloudLayout>
 	);
 }
@@ -39,5 +40,5 @@ export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps
 
 	const { data: files } = await store.dispatch(filesAPI.endpoints.getFiles.initiate('/'));
 	console.log('files', files);
-	return { props: {} };
+	return { props: { files: files || null } };
 });
