6f06a98d0abfc508fee7a315f36930e30a983afb
Стефан Валетов <stormerxl24@gmail.com>
2023-01-20 21:35:34 +0300

    feat(functions): Added utils functions

--- src/client/utils/index.js
+++ src/client/utils/index.js
@@ -1,5 +1,20 @@
 export const tooltipShowDelay = 0.4;
 
+export const getDateStr = (date) => {
+  const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
+  const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
+  const year = date.getFullYear();
+
+  const hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
+  const minutes = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
+
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
+};
+
+export const pxToNumber = (string) => {
+  return Number(string.replace(/px/g, ''));
+};
+
 // export async function hash(string) {
 //   const utf8 = new TextEncoder().encode(string);
 //   const hashBuffer = await crypto.subtle.digest("SHA-256", utf8);
