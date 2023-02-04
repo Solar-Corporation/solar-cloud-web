5cecf89f9e23a14a3dbe08625536756d6e7ea1d2
Стефан Валетов <stormerxl24@gmail.com>
2023-02-04 16:02:36 +0300

    fix(component): Component refactor
    - Removed tooltip
    - Added a view in the context menu
    - Added 'event.stopPropagation' to Click handler

--- src/client/components/UI/Control/index.tsx
+++ src/client/components/UI/Control/index.tsx
@@ -1,26 +1,41 @@
-import { Button, Tooltip } from 'antd';
+import { Button } from 'antd';
 import { FC, ReactNode } from 'react';
-import { tooltipShowDelay } from '../../../utils';
+import styles from '../../../styles/components/Control.module.less';
+
+export interface ControlTypeProps {
+	context?: boolean;
+}
 
 interface ControlProps {
 	icon: ReactNode;
 	title: string;
 	onClick: () => void;
 	children?: never;
+	context?: boolean;
 }
 
-export const Control: FC<ControlProps> = ({ icon, title, onClick }) => {
+export const Control: FC<ControlProps> = ({ icon, title, onClick, context }) => {
+	const handleClick = (event: any) => {
+		event.stopPropagation();
+		onClick();
+	};
+
 	return (
-		<Tooltip
-			title={title}
-			placement="bottom"
-			mouseEnterDelay={tooltipShowDelay}
-		>
+		context
+			?
+			<div
+				className={styles.item}
+				onClick={onClick}
+			>
+				{icon}
+				<span>{title}</span>
+			</div>
+			:
 			<Button
+				title={title}
 				type="ghost"
 				icon={icon}
-				onClick={onClick}
+				onClick={event => handleClick(event)}
 			/>
-		</Tooltip>
 	);
 };
