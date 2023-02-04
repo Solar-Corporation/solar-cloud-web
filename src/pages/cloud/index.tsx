05037eccfb763f1125e8f01e46c55d08e091fc5c
Стефан Валетов <stormerxl24@gmail.com>
2023-02-04 16:00:05 +0300

    fix(page): Page refactor
    - Added getting path and url from the context
    - Added placeholder processing when files are received unsuccessfully
    - Added Control condition processing
    - Removed 'links' placeholder
    - Code cleanup

--- src/pages/cloud/index.tsx
+++ src/pages/cloud/index.tsx
@@ -1,27 +1,132 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { useRouter } from 'next/router';
 import { CloudLayout } from '../../client/components/Cloud/Layout';
-import { Action } from '../../client/components/UI/Action/List';
-import { Control } from '../../client/components/UI/Control/List';
-import { RouteNames } from '../../client/router';
+import Action from '../../client/components/UI/Action/List';
+import Control from '../../client/components/UI/Control/List';
 import { filesAPI } from '../../client/services/FilesService';
 import { wrapper } from '../../client/store';
 import { setInitialUserData } from '../../client/store/reducers/UserSlice';
 import { FileTable } from '../../client/components/FileTable';
+import { useAppSelector } from '../../client/hooks/redux';
+import { getLinks } from '../../client/utils';
+import { setContext } from '../../client/store/reducers/CloudSlice';
 
-export default function Cloud({ files }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-	const id = 3432432;
-	const router = useRouter();
+export default function Cloud({ files, path, links }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	const { selected } = useAppSelector(state => state.cloudReducer);
+
+	if (!files) {
+		files = [{
+			name: 'test',
+			path: '/test',
+			size: '—',
+			fileType: '',
+			mimeType: 'text/plain',
+			isDir: true,
+			isFavorite: false,
+			seeTime: 1673379696000
+		}, {
+			name: 'Архив 1.zip',
+			path: '/Архив 1.zip',
+			size: '10 MiB',
+			fileType: 'zip',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Архив 1.rar',
+			path: '/Архив 1.rar',
+			size: '12 MiB',
+			fileType: 'rar',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Руководство пользователя.pdf',
+			path: '/Руководство пользователя.pdf',
+			size: '5.00 KiB',
+			fileType: 'pdf',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'test123.txt',
+			path: '/test123.txt',
+			size: '7 B',
+			fileType: 'txt',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Старый вариант документа.doc',
+			path: '/Старый вариант документа.doc',
+			size: '10 B',
+			fileType: 'doc',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Новый вариант документа.docx',
+			path: '/Новый вариант документа.docx',
+			size: '10 B',
+			fileType: 'doc',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Таблица 1.xlsx',
+			path: '/Таблица 1.xlsx',
+			size: '10 B',
+			fileType: 'xlsx',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Презентация.pptx',
+			path: '/Таблица 1.pptx',
+			size: '10 B',
+			fileType: 'pptx',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'test123.bsp',
+			path: '/test123.bsp',
+			size: '10 B',
+			fileType: 'bsp',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}, {
+			name: 'Очень длинное, ну прям очень очень длинное, название для изображения, которое не должно поместиться в одну строчку.png',
+			path: '/Очень длинное, ну прям очень очень длинное, название для изображения, которое не должно поместиться в одну строчку.png',
+			size: '10 B',
+			fileType: 'png',
+			mimeType: 'text/plain',
+			isDir: false,
+			isFavorite: false,
+			seeTime: 1673380864000
+		}];
+	}
 
 	const headingOptions = {
-		links: [{ title: 'Все файлы', href: RouteNames.CLOUD }],
+		links,
 		actions: [Action.CREATE],
 		constControls: [Control.VIEW, Control.INFO],
+		floatControls:
+			selected.length
+				? selected.length > 1
+					? [Control.SHARE, Control.DOWNLOAD, Control.DELETE, Control.MOVE, Control.COPY]
+					: [Control.SHARE, Control.DOWNLOAD, Control.DELETE, Control.RENAME, Control.MOVE, Control.COPY, Control.MARK]
+				: path !== '/' ? [Control.SHARE] : undefined,
 		sticky: true
-	};
-
-	const handleClick = async () => {
-		await router.push(`${RouteNames.CLOUD}?path=${id}`);
 	};
 
 	return (
@@ -29,15 +134,18 @@ export default function Cloud({ files }: InferGetServerSidePropsType<typeof getS
 			title="Все файлы"
 			headingOptions={headingOptions}
 		>
-			{/*<button onClick={handleClick}>Открыть папку</button>*/}
 			{files && <FileTable files={files} />}
 		</CloudLayout>
 	);
 }
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(store => async ctx => {
-	setInitialUserData(ctx, store.dispatch);
-	const { data: files } = await store.dispatch(filesAPI.endpoints.getFiles.initiate('/'));
-	console.log('files', files);
-	return { props: { files: files || null } };
+	const { dispatch } = store;
+	const url = decodeURIComponent(ctx.resolvedUrl);
+	const path = ctx.query.path || '/';
+	const links = getLinks(path);
+	setInitialUserData(ctx, dispatch);
+	dispatch(setContext({ url, path }));
+	const { data: files } = await store.dispatch(filesAPI.endpoints.getFiles.initiate(path));
+	return { props: { files: files || null, path, links } };
 });
