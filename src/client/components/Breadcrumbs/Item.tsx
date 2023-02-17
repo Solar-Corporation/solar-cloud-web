80d12fd39fd9ca9baddc924f06613c05a450b895
leankee <leankee217@gmail.com>
2023-02-17 21:05:33 +0300

    fix(logic): Updated logic

--- src/client/components/Breadcrumbs/Item.tsx
+++ src/client/components/Breadcrumbs/Item.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { FC } from 'react';
-import { useAppSelector } from '../../hooks/redux';
 import styles from '../../styles/components/Breadcrumbs.module.less';
 
 interface BreadcrumbsItemProps {
@@ -12,17 +11,11 @@ interface BreadcrumbsItemProps {
 }
 
 export const BreadcrumbsItem: FC<BreadcrumbsItemProps> = ({ link, active }) => {
-	const { url } = useAppSelector(state => state.cloudReducer.context);
-
 	return (
 		<Link
 			href={link.href}
 			title={link.title}
-			className={
-				active
-					? `${styles.item} ${styles.item_active}`
-					: url === link.href ? `${styles.item} ${styles.item_active}` : styles.item
-			}
+			className={active ? `${styles.item} ${styles.item_active}` : styles.item}
 		>
 			{link.title}
 		</Link>
--- src/client/components/Breadcrumbs/index.tsx
+++ src/client/components/Breadcrumbs/index.tsx
@@ -1,5 +1,6 @@
 import { RightOutlined } from '@ant-design/icons';
 import { FC } from 'react';
+import { useAppSelector } from '../../hooks/redux';
 import styles from '../../styles/components/Breadcrumbs.module.less';
 import Control, { ControlList } from '../UI/Control/List';
 import { BreadcrumbsItem } from './Item';
@@ -15,11 +16,13 @@ interface BreadcrumbsProps {
 }
 
 export const Breadcrumbs: FC<BreadcrumbsProps> = ({ links, actions }) => {
+	const { url } = useAppSelector(state => state.cloudReducer.context);
+
 	return (
 		<div className={styles.container}>
 			{links.length > 1
 				? links.map((link, index) => [
-					<BreadcrumbsItem key={index} link={link} />,
+					<BreadcrumbsItem key={index} link={link} active={url === link.href} />,
 					(index !== links.length - 1) && <RightOutlined key={`separator${index}`} className={styles.separator} />])
 				: <BreadcrumbsItem link={links[0]} active />}
 			{actions && actions.length > 0 && <>
