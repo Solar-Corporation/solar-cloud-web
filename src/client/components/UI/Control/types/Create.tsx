c8db01a9218177ab6158766907f34fdccfc1e34c
leankee <leankee217@gmail.com>
2023-02-24 13:32:27 +0300

    feat(logic): Added component logic

--- src/client/components/UI/Control/types/Create.tsx
+++ src/client/components/UI/Control/types/Create.tsx
@@ -1,10 +1,16 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { FC } from 'react';
+import { useAppDispatch } from '../../../../hooks/redux';
+import { setIsContextMenuOpen } from '../../../../store/reducers/CloudSlice';
+import { setIsModalOpen } from '../../../../store/reducers/ModalSlice';
 import { Control, ControlTypeProps } from '../index';
 
 export const ControlCreate: FC<ControlTypeProps> = ({ type, block, className }) => {
+	const dispatch = useAppDispatch();
+
 	const handleClick = () => {
-		console.log('press');
+		dispatch(setIsContextMenuOpen(false));
+		dispatch(setIsModalOpen({ createDirectory: true }));
 	};
 
 	return (
