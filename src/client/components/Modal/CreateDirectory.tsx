4a277acd661ef782f585a7159777558c1b5357f9
leankee <leankee217@gmail.com>
2023-02-24 13:34:06 +0300

    feat(component): Added Modal Create Directory component

---
+++ src/client/components/Modal/CreateDirectory.tsx
@@ -0,0 +1,59 @@
+import { Checkbox, Input } from 'antd';
+import { CheckboxChangeEvent } from 'antd/es/checkbox';
+import { FC, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { IDirectory } from '../../models/IFile';
+import { filesAPI } from '../../services/FilesService';
+import { setIsModalOpen } from '../../store/reducers/ModalSlice';
+import { AppModal } from './index';
+
+export const ModalCreateDirectory: FC = () => {
+	const [name, setName] = useState('');
+	const [relocate, setRelocate] = useState(false);
+	const { path } = useAppSelector(state => state.cloudReducer.context);
+	const { createDirectory } = useAppSelector(state => state.modalReducer.modal);
+	const [createFolder, { isLoading }] = filesAPI.useCreateDirectoryMutation();
+	const dispatch = useAppDispatch();
+
+	const handleUpdate = () => {
+		if (!isLoading) setName('');
+	};
+
+	const handleSubmit = async () => {
+		const directory: IDirectory = { path, name };
+		await createFolder({ directory, relocate });
+	};
+
+	const handleClose = () => {
+		dispatch(setIsModalOpen({ createDirectory: false }));
+	};
+
+	const handleChange = (event: CheckboxChangeEvent) => {
+		setRelocate(event.target.checked);
+	};
+
+	return (
+		<AppModal
+			title="Создать папку"
+			okText="Создать"
+			cancelText="Отменить"
+			open={createDirectory}
+			confirmLoading={isLoading}
+			afterClose={handleUpdate}
+			onOk={handleSubmit}
+			onCancel={handleClose}
+		>
+			<Input
+				name="directoryName"
+				placeholder="Введите название папки"
+				size="large"
+				disabled={isLoading}
+				value={name}
+				onChange={(event) => setName(event.currentTarget.value)}
+			/>
+			<Checkbox checked={relocate} disabled={isLoading} onChange={handleChange}>
+				Перейти в созданную папку
+			</Checkbox>
+		</AppModal>
+	);
+};
