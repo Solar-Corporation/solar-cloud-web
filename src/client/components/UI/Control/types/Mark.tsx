9fc97cd316cced9ea0c567ba13e54506d00fda4c
leankee <leankee217@gmail.com>
2023-02-25 20:14:45 +0300

    feat(logic): Added component logic

--- src/client/components/UI/Control/types/Mark.tsx
+++ src/client/components/UI/Control/types/Mark.tsx
@@ -1,18 +1,36 @@
-import { StarOutlined } from '@ant-design/icons';
-import { FC } from 'react';
+import { StarFilled, StarOutlined } from '@ant-design/icons';
+import { FC, useEffect, useState } from 'react';
+import { useAppSelector } from '../../../../hooks/redux';
+import { filesAPI } from '../../../../services/FilesService';
 import { Control, ControlTypeProps } from '../index';
 
 export const ControlMark: FC<ControlTypeProps> = ({ type, block, className }) => {
-	const handleClick = () => {
-		console.log('press');
+	const [isMarked, setIsMarked] = useState(false);
+	const { selected, marked } = useAppSelector(state => state.cloudReducer);
+	const [markFile, { isLoading: isLoadingMark }] = filesAPI.useMarkFileMutation();
+	const [unmarkFile, { isLoading: isLoadingUnmark }] = filesAPI.useUnmarkFileMutation();
+
+	const handleClick = async () => {
+		const paths = { paths: selected.map(file => file.path) };
+
+		if (isMarked) {
+			await unmarkFile(paths);
+		} else {
+			await markFile(paths);
+		}
 	};
+
+	useEffect(() => {
+		if (selected.length) setIsMarked(!!marked.find(path => path === selected[0].path));
+	}, [selected, marked]);
 
 	return (
 		<Control
-			icon={<StarOutlined />}
-			title="Пометить как избранное"
+			icon={isMarked ? <StarFilled /> : <StarOutlined />}
+			title={isMarked ? 'Убрать из избранного' : 'Пометить как избранное'}
 			onClick={handleClick}
 			className={className}
+			loading={isLoadingMark || isLoadingUnmark}
 			type={type}
 			block={block}
 		/>
