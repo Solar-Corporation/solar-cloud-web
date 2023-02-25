d4744583e34b8110610c5bfad84273cdae2d003b
leankee <leankee217@gmail.com>
2023-02-25 12:59:46 +0300

    feat(component): Added Modal Rename File component

--- src/client/components/Modal/List.tsx
+++ src/client/components/Modal/List.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { ModalCreateDirectory } from './CreateDirectory';
+import { ModalRenameFile } from './RenameFile';
 
 export const ModalList: FC = () => {
 	return (
 		<>
 			<ModalCreateDirectory />
+			<ModalRenameFile />
 		</>
 	);
 };
---
+++ src/client/components/Modal/RenameFile.tsx
@@ -0,0 +1,55 @@
+import { Input } from 'antd';
+import { FC, useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { filesAPI } from '../../services/FilesService';
+import { setIsModalOpen } from '../../store/reducers/ModalSlice';
+import { AppModal } from './index';
+
+export const ModalRenameFile: FC = () => {
+	const { selected } = useAppSelector(state => state.cloudReducer);
+	const [name, setName] = useState('');
+	const { renameFile: isOpen } = useAppSelector(state => state.modalReducer.modal);
+	const [renameFile, { isLoading }] = filesAPI.useRenameFileMutation();
+	const dispatch = useAppDispatch();
+
+	const handleUpdate = () => {
+		if (!isLoading) setName('');
+	};
+
+	const handleSubmit = async () => {
+		const rename = { path: selected[0].path, new_name: name, isDir: selected[0].isDir };
+		await renameFile(rename);
+	};
+
+	const handleClose = () => {
+		dispatch(setIsModalOpen({ renameFile: false }));
+	};
+
+	useEffect(() => {
+		if (selected.length) {
+			if (!name) setName(selected[0].name);
+		}
+	}, [selected]);
+
+	return (
+		<AppModal
+			title={selected.length && selected[0].isDir ? 'Переименовать папку' : 'Переименовать файл'}
+			okText="Переименовать"
+			cancelText="Отменить"
+			open={isOpen}
+			confirmLoading={isLoading}
+			afterClose={handleUpdate}
+			onOk={handleSubmit}
+			onCancel={handleClose}
+		>
+			<Input
+				name="newFileName"
+				placeholder="Введите новое название файла"
+				size="large"
+				disabled={isLoading}
+				value={name}
+				onChange={(event) => setName(event.currentTarget.value)}
+			/>
+		</AppModal>
+	);
+};
