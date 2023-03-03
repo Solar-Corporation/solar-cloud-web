f1192bebacb7ae092bcabf52129ab6125e55ab40
leankee <leankee217@gmail.com>
2023-03-03 09:46:10 +0300

    fix(logic): Updated component logic

--- src/client/components/Breadcrumbs/index.tsx
+++ src/client/components/Breadcrumbs/index.tsx
@@ -2,8 +2,6 @@ import { RightOutlined } from '@ant-design/icons';
 import { FC } from 'react';
 import { useAppSelector } from '../../hooks/redux';
 import styles from '../../styles/components/Breadcrumbs.module.less';
-import { variables } from '../../styles/theme';
-import { pxToNumber } from '../../utils';
 import Control, { ControlList } from '../UI/Control/List';
 import { BreadcrumbsItem } from './Item';
 
@@ -27,9 +25,7 @@ export const Breadcrumbs: FC<BreadcrumbsProps> = ({ links, actions }) => {
 					<BreadcrumbsItem
 						key={index}
 						link={link}
-						style={{
-							maxWidth: `calc((100vw - ${pxToNumber(variables['@space-xl']) * 2 + pxToNumber(variables['@space-xl']) + pxToNumber(variables['@sidebar-width'])}px - ${pxToNumber(variables['@space-6xl']) + 32 * 10 + pxToNumber(variables['@space-xs']) * 10}px) / ${links.length > 4 ? 4 : links.length})`
-						}}
+						style={index === 0 ? { minWidth: 'min-content' } : undefined}
 						active={url === link.href}
 					/>,
 					(index !== links.length - 1) && <RightOutlined key={`separator${index}`} className={styles.separator} />])
