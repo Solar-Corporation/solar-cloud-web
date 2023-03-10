9deff6f51eeaa4dfed49f84313b2ae3eaa2f9624
leankee <leankee217@gmail.com>
2023-03-11 01:26:42 +0300

    feat(logic): Added component logic

--- src/client/components/UI/Control/types/Download.tsx
+++ src/client/components/UI/Control/types/Download.tsx
@@ -1,10 +1,18 @@
 import { DownloadOutlined } from '@ant-design/icons';
 import { FC } from 'react';
+import { useCloudReducer } from '../../../../hooks/cloud';
+import { filesAPI } from '../../../../services/FilesService';
 import { Control, ControlTypeProps } from '../index';
 
 export const ControlDownload: FC<ControlTypeProps> = ({ type, block, className }) => {
+	const { selected } = useCloudReducer();
+	const [downloadFile] = filesAPI.useDownloadFileMutation();
+
 	const handleClick = () => {
-		console.log('press');
+		selected.forEach(async (file) => {
+			const download = { name: file.name, path: file.path };
+			await downloadFile(download);
+		});
 	};
 
 	return (
