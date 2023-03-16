d12207508b3621446eeb3d15ee49e21c1842839e
leankee <leankee217@gmail.com>
2023-03-16 14:31:55 +0300

    feat(page): Added Trash page

--- src/pages/app/trash.tsx
+++ src/pages/app/trash.tsx
@@ -1,12 +1,73 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
 import { CloudLayout } from '../../client/components/Cloud/Layout';
+import { FileTable } from '../../client/components/FileTable';
 import Control from '../../client/components/UI/Control/List';
+import { useCloudReducer } from '../../client/hooks/cloud';
+import { IFile } from '../../client/models/IFile';
 import { RouteNames } from '../../client/router';
+import { filesAPI } from '../../client/services/FilesService';
+import { wrapper } from '../../client/store';
+import { clearSelected, selectFile, unselectFile } from '../../client/store/reducers/CloudSlice';
+import { setInitialUserData } from '../../client/store/reducers/UserSlice';
+import { getFilesPlaceholder } from '../../client/utils';
+import { getFilesContextMenu, getFloatControls } from './files';
 
-export default function CloudTrash() {
+export default function Trash({ files }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	const { selected, marked, dispatch } = useCloudReducer();
+	const router = useRouter();
+	if (!files) files = getFilesPlaceholder();
+
+	const floatControls = getFloatControls(selected);
 	const headingOptions = {
 		links: [{ title: 'Корзина', href: RouteNames.TRASH }],
 		constControls: [Control.VIEW, Control.INFO],
+		floatControls: floatControls,
 		sticky: true
+	};
+
+	const filesContextMenu = getFilesContextMenu(selected);
+
+	const handleRowClick = async (event: any, file: IFile, isSelected: boolean) => {
+		switch (event.detail) {
+			case 1: {
+				if (event.ctrlKey) {
+					if (!isSelected) {
+						dispatch(selectFile(file));
+					} else {
+						dispatch(unselectFile(file));
+					}
+				} else {
+					if (selected.length > 1) {
+						dispatch(clearSelected());
+						dispatch(selectFile(file));
+					} else {
+						if (!isSelected) {
+							dispatch(clearSelected());
+							dispatch(selectFile(file));
+						}
+					}
+				}
+				break;
+			}
+			case 2: {
+				if (file.isDir) {
+					await router.push(`${RouteNames.FILES}/${encodeURIComponent(file.path)}`);
+				} else {
+					console.log('double click!');
+				}
+				break;
+			}
+			default:
+				break;
+		}
+	};
+
+	const handleRowContextMenu = (event: any, file: IFile, isSelected: boolean) => {
+		if (!isSelected) {
+			dispatch(clearSelected());
+			dispatch(selectFile(file));
+		}
 	};
 
 	return (
@@ -14,7 +75,26 @@ export default function CloudTrash() {
 			title="Корзина"
 			headingOptions={headingOptions}
 		>
-			<div style={{ height: '1200px' }} />
+			{files && (
+				<FileTable
+					files={files}
+					contextMenu={filesContextMenu}
+					selected={selected}
+					marked={marked}
+					onRowClick={handleRowClick}
+					onRowContextMenu={handleRowContextMenu}
+				/>
+			)}
 		</CloudLayout>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(store => async ctx => {
+	const { dispatch } = store;
+
+	setInitialUserData(ctx, dispatch);
+
+	const { data: files } = await store.dispatch(filesAPI.endpoints.getTrashFiles.initiate(''));
+
+	return { props: { files: files || null } };
+});
