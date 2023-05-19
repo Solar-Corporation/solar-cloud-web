a247a22d3051dd13a85409e8699cf1966e95653d
leankee <leankee217@gmail.com>
2023-05-19 19:31:29 +0300

    feat(logic): Added component logic

--- src/client/components/UI/Control/types/Share.tsx
+++ src/client/components/UI/Control/types/Share.tsx
@@ -1,20 +1,69 @@
 import { LinkOutlined } from '@ant-design/icons';
-import { FC } from 'react';
+import { Dropdown } from 'antd';
+import { FC, useEffect, useState } from 'react';
+import { useCloudReducer } from '../../../../hooks/cloud';
+import { setIsFilesContextMenuOpen, shareFile } from '../../../../store/reducers/CloudSlice';
 import { Control, ControlTypeProps } from '../index';
+import { ControlCopyLink } from './CopyLink';
+import { ControlDeleteLink } from './DeleteLink';
+import styles from '../../../../styles/components/ContextMenu.module.less';
 
 export const ControlShare: FC<ControlTypeProps> = ({ type, block, className }) => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isShared, setIsShared] = useState(false);
+	const { selected, shared, dispatch } = useCloudReducer();
+
+	const items = [
+		{
+			key: 'copy',
+			label: <ControlCopyLink type="primary" block />,
+			onClick: () => setIsMenuOpen(false)
+		},
+		{
+			key: 'delete',
+			label: <ControlDeleteLink type="primary" block />,
+			onClick: () => setIsMenuOpen(false)
+		}
+	];
+
 	const handleClick = () => {
-		console.log('press');
+		// subject to change
+		if (!isShared) {
+			dispatch(setIsFilesContextMenuOpen(false));
+			dispatch(shareFile(selected[0].path));
+		} else {
+			setIsMenuOpen(true);
+		}
 	};
 
+	const handleOpenChange = (open: boolean) => {
+		if (!open) setIsMenuOpen(false);
+	};
+
+	useEffect(() => {
+		if (selected.length) {
+			setIsShared(!!shared.find(path => path === selected[0].path));
+		}
+	}, [selected, shared]);
+
 	return (
-		<Control
-			icon={<LinkOutlined />}
-			title="Поделиться"
-			onClick={handleClick}
-			className={className}
-			type={type}
-			block={block}
-		/>
+		<Dropdown
+			open={isMenuOpen}
+			trigger={['click']}
+			menu={{ items }}
+			overlayClassName={styles.main}
+			placement="bottomLeft"
+			onOpenChange={handleOpenChange}
+			arrow
+		>
+			<Control
+				icon={<LinkOutlined />}
+				title="Поделиться"
+				onClick={handleClick}
+				className={className}
+				type={type}
+				block={block}
+			/>
+		</Dropdown>
 	);
 };
