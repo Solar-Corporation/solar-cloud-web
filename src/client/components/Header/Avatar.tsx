d36a60ff53a75a5a2574f62e77a012158a8f86a9
leankee <leankee217@gmail.com>
2023-05-19 13:31:59 +0300

    feat(logic): Added user menu

--- src/client/components/Header/Avatar.tsx
+++ src/client/components/Header/Avatar.tsx
@@ -1,13 +1,47 @@
 import { UserOutlined } from '@ant-design/icons';
-import { Avatar } from 'antd';
+import { Avatar, Dropdown, MenuProps } from 'antd';
 import { FC } from 'react';
+import { useAppSelector } from '../../hooks/redux';
 import styles from '../../styles/components/Header.module.less';
+import stylesContextMenu from '../../styles/components/ContextMenu.module.less';
+import { ControlLogout } from '../UI/Control/types/Logout';
 
 export const HeaderAvatar: FC = () => {
+	const { data: user } = useAppSelector(state => state.userReducer);
+
+	const items: MenuProps['items'] = [
+		{
+			key: 'user',
+			label: (
+				<div className={styles.user}>
+					<Avatar
+						icon={<UserOutlined/>}
+					/>
+					<div className={styles.user__label}>
+						<div className={styles.user__name}>{`${user?.fullName.lastName} ${user?.fullName.firstName}`}</div>
+						<div className={styles.user__email}>{user?.email}</div>
+					</div>
+				</div>
+			)
+		},
+		{ type: 'divider' },
+		{
+			key: 'logout',
+			label: <ControlLogout type="primary" block />
+		}
+	];
+
 	return (
-		<div className={styles.avatar} title="Профиль">
-			<Avatar size="large" icon={<UserOutlined />} />
-			<div className={styles.avatarOverlay} />
-		</div>
+		<Dropdown
+			menu={{ items }}
+			trigger={['click']}
+			overlayClassName={stylesContextMenu.main}
+			arrow
+		>
+			<div className={styles.avatar} title="Профиль">
+				<Avatar size="large" icon={<UserOutlined />} />
+				<div className={styles.avatarOverlay} />
+			</div>
+		</Dropdown>
 	);
 };
