0913b357d2ab19b21eefb5cd9457a98f6a2f777d
leankee <leankee217@gmail.com>
2023-05-21 14:06:16 +0300

    feat(hook): Added upload file query hook

---
+++ src/client/hooks/files.ts
@@ -0,0 +1,65 @@
+import Router from 'next/router';
+import { handleApiError, handleApiLoading, handleApiSuccess } from '../components/Notifications';
+import { IUpload } from '../models/IFile';
+import { RouteNames } from '../router';
+import { apiUrl } from '../services/config';
+import { refreshPage } from '../utils';
+
+const getFormData = ({ files, hash, dir }: IUpload) => {
+	const formData = new FormData();
+	if (hash) formData.append('hash', hash);
+	if (dir) formData.append('dir', dir);
+
+	files.forEach((file) => {
+		formData.append('files[]', file);
+	});
+
+	return formData;
+};
+
+export function useUploadFile() {
+	return async (data: IUpload, token: string) => {
+		const key = `${Date.now()}`;
+		const { files } = data;
+		const xhr = new XMLHttpRequest();
+
+		xhr.upload.onloadstart = () => {
+			handleApiLoading({
+				key,
+				message: files.length > 1 ? 'Загрузка файлов...' : 'Загрузка файла...',
+				description: `Загрузка ${files.length > 1 ? `файлов (${files.length})` : 'файла'} в процессе...`
+			});
+		};
+		xhr.onreadystatechange = async () => {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 201) {
+					handleApiSuccess({
+						key,
+						message: 'Загрузка завершена!',
+						description: files.length > 1 ? `Файлы (${files.length}) успешно загружены.` : 'Файл успешно загружен.'
+					});
+				} else {
+					const error = {
+						error: {
+							status: xhr.status,
+							error: JSON.parse(xhr.response).message || 'Произошла непредвиденная ошибка'
+						}
+					};
+					handleApiError(error, key);
+				}
+
+				if (Router.pathname === RouteNames.FILES || Router.pathname === RouteNames.DIRECTORY) {
+					await refreshPage();
+				} else {
+					await Router.push(RouteNames.FILES);
+				}
+			}
+		};
+
+		xhr.open('POST', `${apiUrl}/files`, true);
+		xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+		// xhr.setRequestHeader('Content-Type','multipart/form-data');
+		console.log(xhr.getResponseHeader('Content-Type'));
+		await xhr.send(getFormData(data));
+	}
+}
