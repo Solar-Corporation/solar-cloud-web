33a1db9322b17194c5a6188e833c80a88fe16f9d
leankee <leankee217@gmail.com>
2023-05-22 19:51:20 +0300

    fix(logic): Updated component logic

--- src/client/components/Header/Search.tsx
+++ src/client/components/Header/Search.tsx
@@ -1,13 +1,25 @@
 import { Input } from 'antd';
-import { FC, useState } from 'react';
+import { useRouter } from 'next/router';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { useCloudReducer } from '../../hooks/cloud';
+import { RouteNames } from '../../router';
 import styles from '../../styles/components/Header.module.less';
 
 export const HeaderSearch: FC = () => {
+	const { context } = useCloudReducer();
 	const [value, setValue] = useState('');
+	const router = useRouter();
 
-	const handleSearch = () => {
-		console.log(value);
-		setValue('');
+	useEffect(() => {
+		setValue(context.search);
+	}, [context.search]);
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setValue(event.target.value);
+	};
+
+	const handleSearch = async () => {
+		await router.push(value ? `${RouteNames.SEARCH}?name=${value}` : RouteNames.FILES);
 	};
 
 	return (
@@ -16,7 +28,7 @@ export const HeaderSearch: FC = () => {
 			size="large"
 			placeholder="Поиск"
 			value={value}
-			onChange={(event) => setValue(event.target.value)}
+			onChange={handleChange}
 			onSearch={handleSearch}
 			enterButton
 		/>
--- src/client/components/Header/index.tsx
+++ src/client/components/Header/index.tsx
@@ -9,7 +9,7 @@ import { HeaderSearch } from './Search';
 
 export const Header: FC = () => {
 	const links: IHeaderLink[] = [
-		{ icon: <QuestionOutlined />, title: 'Справка' },
+		// { icon: <QuestionOutlined />, title: 'Справка' },
 		{ icon: <SettingOutlined />, title: 'Настройки', href: RouteNames.SETTINGS }
 	];
 
