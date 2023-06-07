60b68ff3e216653136c36d8908cc42eb8259c7eb
leankee <leankee217@gmail.com>
2023-06-07 19:37:29 +0300

    fix(mutation): Updated mutation

--- src/client/components/Modal/PreviewFile.tsx
+++ src/client/components/Modal/PreviewFile.tsx
@@ -3,16 +3,16 @@ import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import { Button, Modal, Result } from 'antd';
 import React, { FC, useEffect, useState } from 'react';
 import { useCloudReducer } from '../../hooks/cloud';
-import { usePreviewFile } from '../../hooks/files';
 import { useAppSelector } from '../../hooks/redux';
 import { setIsModalOpen } from '../../store/reducers/ModalSlice';
 import styles from '../../styles/components/Modal.module.less';
+import { filesAPI } from '../../services/FilesService';
 
 export const ModalPreviewFile: FC = () => {
 	const { selected, dispatch } = useCloudReducer();
 	const { previewFile: isOpen } = useAppSelector(state => state.modalReducer.modal);
-	// const [previewFile, { data, isLoading }] = filesAPI.usePreviewFileMutation();
-	const { previewFile, uri, isLoading, error } = usePreviewFile();
+	// const [previewFile, { data, isLoading, error }] = filesAPI.useDownloadFileMutation();
+	const [previewFile, { data, isLoading, error }] = filesAPI.usePreviewFileMutation();
 	const [isConverted, setIsConverted] = useState(false);
 
 	const handleClose = () => {
@@ -51,21 +51,24 @@ export const ModalPreviewFile: FC = () => {
 							<div className={styles.preview__empty}>
 								<Result
 									status="error"
-									title={error === 413 ? 'Не поддерживается' : `${error}: Произошла непредвиденная ошибка`}
-									subTitle={error === 413 ? 'Файл слишком большой для предварительного просмотра' : 'Попробуйте перезагрузить страницу'}
+									// @ts-ignore
+									title={error?.error?.status === 413 ? 'Не поддерживается' : `${error}: Произошла непредвиденная ошибка`}
+									// @ts-ignore
+									subTitle={error?.error?.status === 413 ? 'Файл слишком большой для предварительного просмотра' : 'Попробуйте перезагрузить страницу'}
 								/>
 							</div>
 						) : (
 							<DocViewer
 								documents={[{
-									uri,
+									uri: data?.url || '',
 									fileName: selected.length ? selected[0].name : '',
-									fileType: isConverted ? 'text/plain' : undefined
+									fileType: isConverted ? 'text/plain' : undefined,
 								}]}
 								pluginRenderers={DocViewerRenderers}
 								config={{
 									header: { disableHeader: true },
 									pdfVerticalScrollByDefault: true,
+									pdfZoom: { defaultZoom: 0.7, zoomJump: 0.1 },
 									noRenderer: {
 										overrideComponent: () => (
 											<div className={styles.preview__empty}>
@@ -83,8 +86,8 @@ export const ModalPreviewFile: FC = () => {
 													}
 												/>
 											</div>
-										)
-									}
+										),
+									},
 								}}
 							/>
 						)}
